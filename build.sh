df26108114ab9b7534ec5c153e6f58840a5b5a22
nathanaelsanilo <nathanaelsanilo@gmail.com>
2025-06-07 22:41:08 +0700

    chore: add initial project files: .gitignore, build script, and main program

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Change these as needed
+SOURCE_FILE="main.cpp"
+OUTPUT_FILE="main"
+CXX_STANDARD="c++17"
+
+clang++ "$SOURCE_FILE" -o "$OUTPUT_FILE" -std="$CXX_STANDARD" -Wall -Wextra -g
