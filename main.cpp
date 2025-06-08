fdded5ea4304ca7f9dbcc15d8bd969fa9b2aabcb
nathanaelsanilo <nathanaelsanilo@gmail.com>
2025-06-08 21:55:25 +0700

    feat: refactor main function by extracting menu printing and user selection logic

--- main.cpp
+++ main.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
 
-int main()
+void printTitle()
 {
     std::cout << "============================" << std::endl;
     std::cout << "School Management" << std::endl;
     std::cout << "============================" << std::endl;
+}
+
+void printMainMenu()
+{
+    std::cout << "Main menu:" << std::endl;
+    std::cout << "1. Student management" << std::endl;
+    std::cout << "0. Exit" << std::endl;
+}
+
+int getUserSelectMenu()
+{
+    int menus[]{0, 1};
+    int choice;
+    std::cout << "Go to menu : ";
+    std::cin >> choice;
+
+    bool isValidChoice{false};
+    for (int menu : menus)
+    {
+        if (menu == choice)
+        {
+            isValidChoice = true;
+        }
+    }
+
+    if (isValidChoice == false)
+    {
+        std::cout << "Menu is not valid! Try again. ";
+    }
+
+    return choice;
+}
+
+int main()
+{
+    printTitle();
 
     bool isRunning{true};
     while (isRunning)
     {
-        std::cout << "Main menu:" << std::endl;
-        std::cout << "1. Student management" << std::endl;
-        std::cout << "0. Exit" << std::endl;
+        printMainMenu();
 
-        int menus[]{0, 1};
-        int choice;
-        std::cout << "Go to menu : ";
-        std::cin >> choice;
-
-        bool isValidChoice{false};
-        for (int menu : menus)
-        {
-            if (menu == choice)
-            {
-                isValidChoice = true;
-            }
-        }
-
-        if (isValidChoice == false)
-        {
-            std::cout << "Menu is not valid! Try again. ";
-        }
-
+        int choice = getUserSelectMenu();
         if (choice == 0)
         {
             isRunning = false;
