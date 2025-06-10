a48695ccf08c9ee8c146b0ecdd6090e09d22a4f8
nathanaelsanilo <nathanaelsanilo@gmail.com>
2025-06-10 22:07:31 +0700

    feat: enhance student management menu with new options for adding and listing students

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 void printTitle(std::string title);
 void printMainMenu();
@@ -11,6 +12,7 @@ void enterStudentManagementMainMenu();
 
 int main()
 {
+
     printTitle("School Management");
 
     bool isRunning{true};
@@ -91,13 +93,14 @@ void printStudentManagementMainMenu()
     std::cout << std::endl;
     std::cout << "Main menu:" << std::endl;
     std::cout << "1. Register New Student" << std::endl;
+    std::cout << "2. Show Student List" << std::endl;
     std::cout << "0. Exit" << std::endl;
     std::cout << std::endl;
 }
 
 int getUserStudentManagementMenu()
 {
-    int menus[]{0, 1};
+    int menus[]{0, 1, 2};
     int menusSize{sizeof(menus) / sizeof(menus[0])};
     int choice;
     std::cout << "Go to menu : ";
@@ -116,19 +119,39 @@ int getUserStudentManagementMenu()
 void enterStudentManagementMainMenu()
 {
     printTitle("Student Management Main Menu");
+    std::vector<std::string> students{};
     bool isRunning{true};
     while (isRunning)
     {
         printStudentManagementMainMenu();
         int choice = getUserStudentManagementMenu();
-        if (choice == 0)
+        switch (choice)
+        {
+        case 0:
         {
             std::cout << "Exit Student Management." << std::endl;
             isRunning = false;
+            break;
         }
-        else
+        case 1:
         {
-            std::cout << "user choice : " << choice << std::endl;
+            std::string inputStudent{};
+            std::cout << "Insert new student name : " << std::endl;
+            std::cin >> inputStudent;
+            students.push_back(inputStudent);
+            std::cout << "Data inserted" << std::endl;
+            break;
+        }
+        case 2:
+        {
+            std::cout << "List of students : " << std::endl;
+            for (size_t i{0}; i < students.size(); ++i)
+            {
+                std::cout << (i + 1) << ". " << students.at(i) << std::endl;
+            }
+        }
+        default:
+            break;
         }
     }
 }
