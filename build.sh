a8bd85fcf49dadee303d791e76f2432222bc8c4c
nathanaelsanilo <nathanaelsanilo@gmail.com>
2025-07-20 22:02:18 +0700

    build: include print cpp file

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Change these as needed
-SOURCE_FILE="main.cpp"
+SOURCE_FILE="main.cpp print.cpp"
 OUTPUT_FILE="main"
 CXX_STANDARD="c++17"
 
-clang++ "$SOURCE_FILE" -o "$OUTPUT_FILE" -std="$CXX_STANDARD" -Wall -Wextra -g
+clang++ $SOURCE_FILE -o "$OUTPUT_FILE" -std="$CXX_STANDARD" -Wall -Wextra -g
